943b23e6de868659eeed3f98f70e3bb6dc62cc46
Ada Cieńciała <57063056+adacienciala@users.noreply.github.com>
2022-03-29 22:15:09 +0200

    Login screen (#2)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import { Button, Center } from '@mantine/core';
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Center style={{ height: '100vh' }}>
-      <Button>Hello world!</Button>
-    </Center>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/auth" element={<Auth />} />
+        <Route
+          path="/"
+          element={
+            <RequireAuth>
+              <Home />
+            </RequireAuth>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  const location = useLocation();
+  const logged = localStorage.getItem("token");
+  if (!logged) {
+    return <Navigate to="/auth" state={{ from: location }} replace />;
+  }
+  return children;
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { MantineProvider } from '@mantine/core';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { MantineProvider } from "@mantine/core";
+import "./App.css";
+import { NotificationsProvider } from "@mantine/notifications";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,19 +11,28 @@ ReactDOM.render(
       theme={{
         colors: {
           // you can generate it with those: https://mantine.dev/theming/extend-theme/#extend-or-replace-colors
-          yellow: ['#FFF9C5', '#FFF7B2', '#FFF69F', '#FFF48B', '#FFF278', '#FFEF5B', '#FFEC3E', '#f7e436', '#f2df30','#ebca28' ],
+          yellow: [
+            "#FFF9C5",
+            "#FFF7B2",
+            "#FFF69F",
+            "#FFF48B",
+            "#FFF278",
+            "#FFEF5B",
+            "#FFEC3E",
+            "#f7e436",
+            "#f2df30",
+            "#ebca28",
+          ],
         },
         primaryColor: "yellow",
-        fontFamily: 'Montserrat, sans-serif',
+        fontFamily: "Montserrat, sans-serif",
+        colorScheme: "dark",
       }}
     >
-      <App />
+      <NotificationsProvider>
+        <App />
+      </NotificationsProvider>
     </MantineProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
