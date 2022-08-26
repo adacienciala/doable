2f5e3732bf8176902217bd94bcf946da1e3779b9
adacienciala <adacien@o2.pl>
2022-08-26 16:07:01 +0200

    Make userCluster dynamic

--- src/containers/UserCluster/index.tsx
+++ src/containers/UserCluster/index.tsx
@@ -1,25 +1,30 @@
 import { Avatar, Tooltip } from "@mantine/core";
 
+const usersShownNumber = 2;
+
 export const UserCluster = ({ users }: { users: string[] }) => {
-  const usersShown = users.slice(0, 3);
-  const usersHidden = users.slice(3);
+  const usersShown = users.slice(0, usersShownNumber);
+  const usersHidden =
+    users.length > usersShownNumber ? users.slice(usersShownNumber) : null;
   return (
     <>
       <Tooltip.Group openDelay={300} closeDelay={100}>
         <Avatar.Group spacing="sm">
           {usersShown.map((u: string) => (
-            <Tooltip label={u} withArrow>
+            <Tooltip key={u} label={u} withArrow>
               <Avatar radius="xl" />
             </Tooltip>
           ))}
-          <Tooltip
-            withArrow
-            label={usersHidden.map((u: string) => (
-              <div>{u}</div>
-            ))}
-          >
-            <Avatar radius="xl">+2</Avatar>
-          </Tooltip>
+          {usersHidden && (
+            <Tooltip
+              withArrow
+              label={usersHidden.map((u: string) => (
+                <div>{u}</div>
+              ))}
+            >
+              <Avatar radius="xl">{`+${usersHidden.length}`}</Avatar>
+            </Tooltip>
+          )}
         </Avatar.Group>
       </Tooltip.Group>
     </>
