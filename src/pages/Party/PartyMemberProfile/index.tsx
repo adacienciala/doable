4623db013fdb08a795bcfb2dd940a8d11cff5c40
adacienciala <adacien@o2.pl>
2022-08-28 20:14:32 +0200

    added some numbers lol

--- src/pages/Party/PartyMemberProfile/index.tsx
+++ src/pages/Party/PartyMemberProfile/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Group, Stack } from "@mantine/core";
+import { Group, Stack, Text } from "@mantine/core";
 import { Profile } from "../../../components/Profile";
 import { IUser } from "../../../models/user";
 
@@ -22,13 +22,19 @@ export const PartyMemberProfile = ({ user }: { user?: IUser }) => {
             })}
           />
         </Stack>
-        <Box
-          sx={{
-            backgroundColor: "yellow",
+        <Stack
+          align="center"
+          sx={(theme) => ({
+            backgroundColor: theme.colors.gray[0],
             borderRadius: "10px",
             width: "40px",
-          }}
-        ></Box>
+            paddingTop: "10px",
+          })}
+        >
+          <Text weight="bold" color="black">
+            {Math.floor(Math.random() * 10)}
+          </Text>
+        </Stack>
       </Group>
     </>
   );
